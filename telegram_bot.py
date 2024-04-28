626e4fd704002539951c3dad4eff5eff63f4e9c8
MolokoMangement <168341907+MolokoMangement@users.noreply.github.com>
2024-04-28 16:51:26 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,14 +1,22 @@
-from flask import Flask, request
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler
+from flask import Flask, request, jsonify
+from telegram import Update, Bot, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 import os
-import threading
 
-app = Flask(__name__)  # Create a Flask app
+app = Flask(__name__)
+TOKEN = "7128626321:AAEiBR9_wCkYBXmNRv0V5gZfxKs7h65cw5c"
+bot = Bot(token=TOKEN)
+URL = "https://molokomanagement-1ca2f474c16e.herokuapp.com/"
 
 @app.route('/')
 def index():
-    return 'The bot is running!'  # Simple route to show the bot is running
+    return 'The bot is running!'
+
+@app.route(f"/{TOKEN}", methods=['POST'])
+def webhook():
+    update = Update.de_json(request.get_json(force=True), bot)
+    dispatcher.process_update(update)
+    return jsonify({"status": "ok"})
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [[InlineKeyboardButton("Покажите мне как (на русском)🇷🇺", callback_data='1')],
@@ -35,21 +43,21 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Opening {url} in another chat.")
             await context.bot.send_message(chat_id='@DavidBeaumont', text="Hello from the bot!")
         else:
-            await query.message.reply_text(f"Opening {url}")
+            await query.edit_message_text(text=f"Opening {url}")
             await context.bot.send_message(chat_id=update.effective_chat.id, text=url)
 
-def run_flask():
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, use_reloader=False)
+def main():
+    global dispatcher
+    application = Application.builder().token(TOKEN).build()
+    dispatcher = application.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CallbackQueryHandler(button))
 
-def run_bot():
-    application = Application.builder().token("7128626321:AAEiBR9_wCkYBXmNRv0V5gZfxKs7h65cw5c").build()
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CallbackQueryHandler(button))
-    application.run_polling()
+    # Set webhook
+    application.run_webhook(listen="0.0.0.0",
+                            port=int(os.environ.get('PORT', 5000)),
+                            url_path=TOKEN,
+                            webhook_url=URL + TOKEN)
 
 if __name__ == '__main__':
-    # Run Flask and Telegram bot in separate threads
-    threading.Thread(target=run_flask).start()
-    threading.Thread(target=run_bot).start()
-
+    main()
