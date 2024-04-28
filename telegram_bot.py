fe092f53a00caad801ff53e8eb8bbda816059b48
MolokoMangement <168341907+MolokoMangement@users.noreply.github.com>
2024-04-28 16:20:05 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,13 @@
+from flask import Flask, request
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler
+import os
+
+app = Flask(__name__)  # Create a Flask app
+
+@app.route('/')
+def index():
+    return 'The bot is running!'  # Simple route to show the bot is running
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [[InlineKeyboardButton("Покажите мне как (на русском)🇷🇺", callback_data='1')],
@@ -16,18 +24,16 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     urls = {
         '1': 'https://pitch.com/v/pd-in-russian-imrthj',
         '2': 'https://pitch.com/v/pd-in-ukrainian-nvkuc9',
-        '3': 'https://t.me/DavidBeaumont'  # URL for option 3
+        '3': 'https://t.me/DavidBeaumont'
     }
 
     url = urls.get(selected_option)
 
     if url:
         if selected_option == '3':
-            # For option 3, send a message to another chat
             await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Opening {url} in another chat.")
             await context.bot.send_message(chat_id='@DavidBeaumont', text="Hello from the bot!")
         else:
-            # For options 1 and 2, send the URL to the current chat
             await query.message.reply_text(f"Opening {url}")
             await context.bot.send_message(chat_id=update.effective_chat.id, text=url)
 
@@ -37,6 +43,9 @@ def main() -> None:
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CallbackQueryHandler(button))
 
+    # Set Flask to run on the port assigned by Heroku and start the Telegram bot
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, use_reloader=False)
     application.run_polling()
 
 if __name__ == '__main__':
